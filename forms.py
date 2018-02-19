7d245e6cbc28bae9908a6759abaef106bc5660e2
gouravd8a <gouravjjw@d8a.ai>
2018-02-20 00:51:10 +0530

    mod1

--- forms.py
+++ forms.py
@@ -1,25 +1,13 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import TextField,PasswordField
 
-class UserLoginForm(Form):
+class UserLoginForm(FlaskForm):
     """Creates a form which will allow the user to log into the system."""
 
     username = TextField()
     password = PasswordField()
 
-    def clean(self):
-        super(UserLoginForm, self).clean()
-        try:
-            u_name, pwd = self.cleaned_data["username"],\
-                          self.cleaned_data["password"]
-            user = authenticate(username=u_name, password=pwd)
-        except Exception:
-            raise forms.ValidationError(
-                "Username and/or Password is not entered"
-            )
-        if not user:
-            raise forms.ValidationError("Invalid username/password")
-        return user
+
 
 
 
--- views.py
+++ views.py
@@ -1,15 +1,33 @@
 from flask import Flask,request,render_template
 from forms import UserLoginForm
+import sqlite3 as sql
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 
-@app.route('/')
+@app.route('/',methods = ['POST', 'GET'])
 def user_login():
     """Take the credentials of the user and log the user in."""
-    form = UserLoginForm()
+    if request.method == 'POST':
+   
+    	user = request.form['username']
+    	password = request.form['password']
+    	
+    	conn = sql.connect('db.sqlite3')
+    	conn.row_factory = sql.Row
 
+    	cursor = conn.execute("select * from auth_user")
+   
+    	for row in cursor:
+    			uname = row["first_name"]
+    			pword = row["last_name"]
+    			uid = row["id"]
+    			if uname == user and pword == password:
+    				return render_template('yaksh/hello.html')
+
+    form = UserLoginForm()
     return render_template('yaksh/login.html', form=form)
 
 
