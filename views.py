e828d263bd9937851bde5a64523fdb351cdacbe7
gouravd8a <35448986+gouravd8a@users.noreply.github.com>
2018-03-17 20:38:52 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,9 +1,12 @@
-from flask import Flask,request,render_template,redirect,url_for,flash
+from flask import Flask,request,render_template,redirect,url_for,flash,send_file
 from forms import UserLoginForm,CourseForm,QuizForm,UserRegisterForm,QuestionFilterForm
 from wtforms import DateTimeField
 import sqlite3 as sql
 from datetime import datetime
 from textwrap import dedent
+import pytz
+import time
+from pytz import timezone
 
 app = Flask(__name__)
 
@@ -100,8 +103,10 @@ def course_module(cid):
 		lid = l['learningmodule_id']
 		record = conn1.execute("select * from yaksh_learningmodule where id = ?",(lid,))
 		lmod.append(record)
-	print(lmod)
-	return render_template('yaksh/course_modules.html',course=c,learning_modules=lmod)
+	#print(lmod)
+	return render_template('yaksh/course_modules.html',course=c,learning_modules=lmod,user ="Student")
+
+
 @app.route('/exam/manage/courses/all_quizzes/',methods = ['POST', 'GET'])
 def show_all_quizzes():
 	conn1 = sql.connect('db.sqlite3')
@@ -109,8 +114,204 @@ def show_all_quizzes():
 	quizzes = conn1.execute("select * from yaksh_quiz")
 	return render_template('yaksh/courses.html',quizzes = quizzes,type = "quiz")
 
+@app.route('/exam/quizzes/view_module/<mid>/<cid>')
+def view_module(mid,cid,msg= "none"):
+	conn1 = sql.connect('db.sqlite3')
+	conn1.row_factory = sql.Row
+	all_modules = []
+	if msg=="none":
+		msg =""
+	module = conn1.execute("select * from yaksh_learningmodule where id =?",(mid,))
+	for mod in module:
+		learning_module = mod
+	record = conn1.execute("select * from yaksh_course where id =?",(cid,))
+	for cou in record:
+		course = cou
+	records = conn1.execute("select * from yaksh_course_learning_module where course_id = ?",(cid,))
+	for r in records:
+		ycm = r['learningmodule_id']
+		record = conn1.execute("select * from yaksh_learningmodule where id = ?",(ycm,))
+		for rec in record:
+			all_modules.append(rec)
 
 
+	record = conn1.execute("select * from yaksh_learningmodule_learning_unit where learningmodule_id = ? limit 1",(mid,))
+	for rec in record:
+		first_unit = rec['learningunit_id']
+	return render_template('yaksh/show_video.html',msg = msg,learning_module = learning_module,state ="module",course=course,all_modules = all_modules,first_unit=first_unit)
+
+def has_questions(qpid):
+	conn1 = sql.connect('db.sqlite3')
+	conn1.row_factory = sql.Row
+	records = conn1.execute("select * from yaksh_questionpaper_fixed_questions where questionpaper_id = ?",(qpid,))
+	for record in records:
+		if record:
+			return "true"
+	return "false"
+
+
+def show_question(question,last_attempt,cid,mid,qpid,quiz,previous_question = None):
+	quiz_type = 'Exam'
+	can_skip = False
+	if previous_question:
+		delay_time = time_left_on_question(last_attempt,previous_question)
+	else:
+		delay_time = time_left_on_question(last_attempt,question)
+	if previous_question and quiz['is_exercise']:
+		if delay_time <= 0 or previous_question in questions_answered(last_attempt):
+			can_skip = True
+		question = previous_question
+	if not question:
+		msg = 'Congratulations!  You have successfully completed the quiz.'
+		return complete()
+
+	if not quiz['active']:
+		reason = 'The quiz has been deactivated!'
+		return complete()
+
+	if not quiz['is_exercise']:
+		if paper.time_left() <= 0:
+			reason = 'Your time is up!'
+			return complete()
+	else:
+		quiz_type = 'Exercise'
+	if question in questions_answered(last_attempt):
+		notification = (  'You have already attempted this question successfully'
+            if question['type'] == "code" else
+            'You have already attempted this question')
+	return "hi"
+          
+
+     
+@app.route('/exam/start/<qpid>/<mid>/<cid>/<attempt_num>')
+@app.route('/exam/start/<qpid>/<mid>/<cid>')
+def start(qpid,mid,cid,attempt_num=None):
+	conn1 = sql.connect('db.sqlite3')
+	conn1.row_factory = sql.Row
+	userid = 1
+	status = has_questions(qpid)
+	if status == "false":
+		return view_module(mid = mid,cid = cid ,msg="Quiz does not have Questions, please contact your '\
+            'instructor/administrator.")
+	records = conn1.execute("select * from yaksh_questionpaper where id =?",(qpid,))
+	for record in records:
+		question_paper = record
+		quizid = record['quiz_id']
+	records = conn1.execute("select * from yaksh_quiz where id =?",(quizid,))
+	for reco in records:
+		quiz = reco
+		status1= expired(quiz)
+		if status1 == "yes":
+			return view_module(mid = mid,cid = cid ,msg="Quiz has expired.")
+	last_attempt=get_user_last_attempt(qpid,userid,cid)
+	if last_attempt and is_attempt_inprogress(last_attempt):
+		print("hi")
+		return show_question(
+            question =current_question(last_attempt), last_attempt=last_attempt,
+            cid = cid,mid =mid, qpid =qpid,quiz=quiz,
+            previous_question=current_question(last_attempt)
+        )
+	timezone = pytz.utc
+	return render_template('yaksh/intro.html',question_paper = question_paper,quiz= quiz,timezone = timezone,user = "Student",qpid= qpid, mid =mid,cid =cid)
+
+
+def get_user_last_attempt(qpid,userid,cid):
+	conn1 = sql.connect('db.sqlite3')
+	conn1.row_factory = sql.Row
+	records = conn1.execute("select * from yaksh_answerpaper where question_paper_id = ? and course_id = ? and user_id = ? order by attempt_number desc",(qpid,cid,userid,))
+	for record in records:
+		last_attempt = record
+		return last_attempt
+
+def is_attempt_inprogress(attempt):
+    if attempt['status'] == 'inprogress':
+        return time_left(attempt) > 0
+
+def time_left(attempt):
+
+    secs = get_total_seconds(attempt)
+    apid = attempt['id']
+    conn1 = sql.connect('db.sqlite3')
+    conn1.row_factory = sql.Row
+    records = conn1.execute("select * from yaksh_answerpaper where id = ?",(apid,))
+    for record in records:
+    	qpid = record['question_paper_id']
+    	records2= conn1.execute("select * from yaksh_questionpaper where id = ?",(qpid,))
+    	for record2 in records2:
+    		quizid = record2['quiz_id']
+    		records3= conn1.execute("select * from yaksh_quiz where id = ?",(quizid,))
+    		for record3 in records3:
+    			qduration = record3['duration']
+				
+    total = qduration*60.0
+    remain = max(total - secs, 0)
+    return int(remain)
+
+def time_left_on_question(attempt,question):
+    secs = get_total_seconds(attempt)
+    total = question['min_time']*60.0
+    remain = max(total - secs, 0)
+    #print(remain)
+    return int(remain)
+def current_question(attempt):
+
+    conn1 = sql.connect('db.sqlite3')
+    conn1.row_factory = sql.Row
+    apid = attempt['id']
+    questions1 =[]
+    questions2 = []
+    flag =0
+    records = conn1.execute("select * from yaksh_answerpaper_questions_unanswered where answerpaper_id = ?",(apid,))
+    for record in records:
+    	questions1.append(record['question_id'])
+    	if record:
+    		flat =1
+    records2 = conn1.execute("select * from yaksh_answerpaper_questions where answerpaper_id = ?",(apid,))
+    for record2 in records2:
+    	questions2.append(record2['question_id'])
+    if flag == 0:
+    	cur_question = get_current_question(attempt,questions2)
+    else:
+    	cur_question = get_current_question(attempt,questions1)
+    return cur_question
+
+def questions_answered(attempt):
+
+    conn1 = sql.connect('db.sqlite3')
+    conn1.row_factory = sql.Row
+    apid = attempt['id']
+    questions1 =[]
+    records2 = conn1.execute("select * from yaksh_answerpaper_questions_answered where answerpaper_id = ?",(apid,))
+    for record2 in records2:
+    	questions1.append(record2['question_id'])
+    return questions1
+
+def get_current_question(attempt,questions):
+
+    conn1 = sql.connect('db.sqlite3')
+    conn1.row_factory = sql.Row
+    #print("jo")
+    ordered_question_ids = [int(q) for q in attempt['questions_order'].split(',')]
+
+	
+    for qid in ordered_question_ids:
+        if qid in questions:
+            records = conn1.execute("select * from yaksh_question where id = ?",(qid,))
+            for record in records:
+            	question = record
+            	#print(record['summary'])
+            	return question
+
+
+
+def get_total_seconds(attempt):
+
+
+	t = str(datetime.now())
+	nowtime = t[0:t.find(".")]
+	dt = datetime.strptime( attempt['end_time'],'%Y-%m-%d %H:%M:%S') - datetime.strptime(attempt['start_time'],'%Y-%m-%d %H:%M:%S')
+	secs = dt.seconds + dt.days*24*3600
+	return secs
 
 
 def ismoderator(conn,uid):
@@ -145,13 +346,92 @@ def add_lesson():
 	return render_template('yaksh/add_lesson.html')
 
 
+@app.route('/data/<filename>')
+def data(filename):
+	file = filename
+	return send_file('static/'+file, attachment_filename=file)
+
 @app.route('/exam/manage/addquiz/')
 def add_quiz():
 
 	form = QuizForm()
 	return render_template('yaksh/add_quiz.html',form = form)
 
+@app.route('/exam/show_lesson/<lid>/<mid>/<cid>/')
+def show_lesson(lid,mid,cid):
 
+	conn1 = sql.connect('db.sqlite3')
+	conn1.row_factory = sql.Row
+	all_modules = []
+	module = conn1.execute("select * from yaksh_learningmodule where id =?",(mid,))
+	for mod in module:
+		learning_module = mod
+	record = conn1.execute("select * from yaksh_course where id =?",(cid,))
+	for cou in record:
+		course = cou
+	records = conn1.execute("select * from yaksh_course_learning_module where course_id = ?",(cid,))
+	for r in records:
+		ycm = r['learningmodule_id']
+		record = conn1.execute("select * from yaksh_learningmodule where id = ?",(ycm,))
+		for rec in record:
+			all_modules.append(rec)
+	record = conn1.execute("select * from yaksh_lesson where id =?",(lid,))
+	for r in record:
+		lesson =r
+	record = conn1.execute("select * from yaksh_learningunit where lesson_id =?",(lid,))
+	for r in record:
+		current_unit =r
+	return render_template('yaksh/show_video.html',current_unit = current_unit,lesson =lesson,state = "lesson",course = course, all_modules= all_modules, learning_module = learning_module)
+
+
+@app.route('/exam/next_unit/<cid>/<mid>/<uid>/1/')
+def get_nextunit(cid,mid,uid):
+	
+	conn = sql.connect('db.sqlite3')
+	conn.row_factory = sql.Row
+	records = conn.execute("select * from yaksh_learningunit where id = ?",(uid,))
+	for rec in records:
+		utype = rec['type']
+		if utype == "lesson":
+			lessid = rec['lesson_id']
+			return redirect('/exam/show_lesson/{0}/{1}/{2}'.format(
+            lessid, mid, cid))
+
+
+@app.route('/exam/next_unit/<cid>/<mid>/<cuid>')
+def get_next_unit(cid,mid,cuid):
+	conn = sql.connect('db.sqlite3')
+	conn.row_factory = sql.Row	
+	uid =[]
+	currid = int(cuid)
+	records= conn.execute("select * from yaksh_learningmodule_learning_unit where learningmodule_id = ?",(mid,))
+	for r in records:
+		uid.append(r['learningunit_id'])
+		
+	for a in range(len(uid)):
+
+		if uid[a] == currid:
+			ind = a+1
+			if ind == len(uid):
+				ind = 0
+			break
+	next_unit_id = uid[ind]
+
+	records = conn.execute("select * from yaksh_learningunit where id = ?",(next_unit_id,))
+	for rec in records:
+		utype = rec['type']
+		if utype == "lesson":
+			lessid = rec['lesson_id']
+			return redirect('/exam/show_lesson/{0}/{1}/{2}'.format(
+            lessid, mid, cid))
+		elif utype == "quiz":
+			quizid = rec['quiz_id']
+			records = conn.execute("select * from yaksh_questionpaper where quiz_id = ?",(quizid,))
+			for r in records:
+				qpid = r['id']
+				return redirect('/exam/start/{0}/{1}/{2}'.format(
+            qpid, mid, cid))
+	return "Sorry the quiz is not ready"
 
 @app.route('/exam/manage/designquestionpaper/<qid>/<qpid>/')
 def design_questionpaper(qid,qpid):
@@ -225,7 +505,6 @@ def get_creator_name(course):
 	records = conn.execute("select * from yaksh_course where id = ?",(cid,))
 	for record in records:
 		crid = 3
-	print(crid)
 	records = conn.execute("select * from auth_user where id = ?",(crid,))
 	for record in records:
 		name.append(record['first_name']+" "+record['last_name'])
@@ -239,6 +518,82 @@ def get_question_paper(quiz):
 	qid = quiz['id']
 	records = conn.execute("select * from yaksh_questionpaper where quiz_id = ?",(qid,))
 	return records
+
+@app.template_filter('get_question_paper')
+def get_questionpaper(uid):
+	conn = sql.connect('db.sqlite3')
+	conn.row_factory = sql.Row
+	qpid = []
+	record = conn.execute("select * from yaksh_learningunit where id = ?",(uid,))
+	for r in record:
+		qid = r['quiz_id']
+	records = conn.execute("select * from yaksh_questionpaper where quiz_id = ?",(qid,))
+	for r in records:
+		qpid.append(r['id'])
+		return qpid
+
+
+
+
+@app.template_filter('has_question_paper')
+def has_questionpaper(uid):
+	conn = sql.connect('db.sqlite3')
+	conn.row_factory = sql.Row
+	stat = []
+	record = conn.execute("select * from yaksh_learningunit where id = ?",(uid,))
+	for r in record:
+		qid = r['quiz_id']
+	records = conn.execute("select * from yaksh_questionpaper where quiz_id = ?",(qid,))
+	for record in records:
+		stat.append("true")
+		return stat
+	stat.append("false")
+	return stat
+
+@app.template_filter('is_expired')
+def is_expired(quiz):
+	stat = []
+	t = str(datetime.now())
+	nowtime = t[0:t.find(".")]
+	if time.strptime(quiz['start_date_time'],'%Y-%m-%d %H:%M:%S') <= time.strptime(nowtime,'%Y-%m-%d %H:%M:%S') < time.strptime(quiz['end_date_time'],'%Y-%m-%d %H:%M:%S'):
+		stat.append("no")
+	else:
+		stat.append("yes")
+	return stat
+
+
+def expired(quiz):
+	stat = []
+	t = str(datetime.now())
+	nowtime = t[0:t.find(".")]
+
+	if time.strptime(quiz['start_date_time'],'%Y-%m-%d %H:%M:%S') <= time.strptime(nowtime,'%Y-%m-%d %H:%M:%S') < time.strptime(quiz['end_date_time'],'%Y-%m-%d %H:%M:%S'):
+		return "no"
+	else:
+		return "yes"
+
+@app.template_filter('has_file')
+def has_file(lid):
+	conn = sql.connect('db.sqlite3')
+	conn.row_factory = sql.Row
+	stat = []
+	record = conn.execute("select * from yaksh_lessonfile where lesson_id = ?",(lid,))
+	for r in record:
+		stat.append("true")
+		return stat
+	stat.append("false")
+	return stat
+
+
+@app.template_filter('get_file_name')
+def get_file_name(lid):
+	conn = sql.connect('db.sqlite3')
+	conn.row_factory = sql.Row
+	stat = []
+	record = conn.execute("select * from yaksh_lessonfile where lesson_id = ?",(lid,))	
+	for r in record:
+		stat.append(r['file'])
+	return stat
 
 @app.template_filter('get_questionpaperstatus')
 def get_question_paper_status(quiz):
@@ -299,7 +654,6 @@ def get_unit_status(lid):
 				cmod=cmod+1
 
 	if tmod == 0:
-		print("hi")
 		status= "not attempted"
 	elif tmod == cmod:
 		status ="completed"
@@ -370,8 +724,6 @@ def get_course_details(course):
 			ts.append(get_total_students(q1,cid))
 			ps.append(get_passed_students(q1,cid))
 			fs.append(get_failed_students(q1,cid))
-	print(ps)
-	print(fs)
 	return [(qname,ts,ps,fs)]
 
 
